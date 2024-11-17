4aca6c70f19b6c72a4c5f6dabd577427c63bd630
Subdij <ziyadou2011@hotmail.fr>
2024-11-17 19:03:30 +0100

    first commit

---
+++ Pages/Header.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Bienvenue à l'Hotel Paie</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="Pages/Chambres.php">Chambres</a></li>
+            <li><a href="Pages/Reservations.php">Réservations</a></li>
+        </ul>
+    </nav>
+</header>
